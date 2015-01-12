5cd60623d43ae2c330a41d39ba9934ee3f61e726
saingly <saingly9@gmail.com>
2015-01-12 11:15:18 -0800

    add more class

---
+++ ProjectGIT/Program.cs.REMOTE.5928.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectGIT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myname = "Saingly";
+            Console.Write(myname);
+            string myUserName ="tyhor";
+            Console.Write(myUserName);
+        }
+    }
+}
