887c0ce457939c3c270f8708d52c0693e2d59bf2
saingly <saingly9@gmail.com>
2015-01-12 10:18:47 +0700

    new code for saingly7

---
+++ ProjectGIT/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectGIT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         // saingly7 code
+            string nameA = "saingly7";
+            Console.WriteLine(nameA);
+
+        }
+    }
+}
