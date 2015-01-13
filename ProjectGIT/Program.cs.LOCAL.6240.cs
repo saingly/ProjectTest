85799e1ac2cdacf79781b5a6fb48241d7862afa4
saingly <saingly9@gmail.com>
2015-01-12 17:18:54 -0800

    add new Crsytal Report

---
+++ ProjectGIT/Program.cs.LOCAL.6240.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectGIT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*-------Saingly7-------*/
+
+           // saingly7 code
+            string nameA = "saingly7";
+            Console.WriteLine(nameA);
+
+            //saingly7 add his age
+            byte saingly7Age = 23;
+            Console.WriteLine(saingly7Age);
+           
+
+            /*-------Admin---------*/
+
+            // admin add his age
+            byte adminAge = 24;
+            Console.WriteLine(adminAge);
+
+
+            // add admin name
+            string adminName = "saingly";
+            Console.WriteLine(adminName);
+
+        
+
+           
+
+            //saingly7 comment
+
+
+        }
+    }
+}
---
+++ ProjectGIT/Program.cs.REMOTE.6240.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectGIT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myname = "Saingly";
+            Console.Write(myname);
+            
+           string co="it programer in hong hout";
+            Console.Write(co);
+        }
+    }
+}
